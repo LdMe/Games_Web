4203e63cd2212b4e3e60568818efb9fd24222ec1
LdMe <dlafuente003@gmail.com>
2020-12-17 09:59:07 +0100

    deployment modified

--- create_games.sh
+++ create_games.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 domain1="games.lafuentedanel.com"
 docker build -t mygames .
-docker run --name $domain1 --net dockerwp  -e VIRTUAL_HOST=$domain1,www.$domain1 -e LETSENCRYPT_HOST=$domain1,www.$domain1 -e LETSENCRYPT_EMAIL=admin@$domain1 -d --restart always mygames
+docker run --name $domain1 --net dockerwp  -v ~/Games_Web:/usr/share/nginx/html -e VIRTUAL_HOST=$domain1,www.$domain1 -e LETSENCRYPT_HOST=$domain1,www.$domain1 -e LETSENCRYPT_EMAIL=admin@$domain1 -d --restart always mygames
